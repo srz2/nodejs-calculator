63a352f6e8682c0b77cedc3ef05f26f94ede79f9
Steven Zilberberg <srz222@gmail.com>
2019-11-07 10:10:50 -0500

    Added unit test for calculation

---
+++ test/calculator.js
@@ -0,0 +1,79 @@
+const chai = require('chai');
+
+describe('Calculator Logic', function() {
+
+    const calc = require('../logic/calculator');
+
+    describe('Addition Calculation', function() {
+        it('Addition', function() {
+            const result = calc.addition(5, 6);
+            chai.assert.equal(result, 11, 'Addition Fails');
+        });    
+    });
+
+    describe('Subtraction Calculation', function() {
+        it('Subtraction with larger Minuend', function() {
+            const result = calc.subtraction(10, 6);
+            chai.assert.equal(result, 4, 'Subtraction Fails: Expecting 4');
+        });
+
+        it('Subtraction with equal number', function() {
+            const result = calc.subtraction(5, 5);
+            chai.assert.equal(result, 0, 'Subtraction Fails: Execpting 0');
+        });
+
+        it('Subtraction with smaller Minuend', function() {
+            const result = calc.subtraction(5, 20);
+            chai.assert.equal(result, -15, 'Subtraction Fails: Expecting -15');
+        });
+    });
+
+    describe('Multiplication Calculation', function() {
+        it('Multiply with 0', function() {
+            const result = calc.multiply(5, 0);
+            chai.assert.equal(result, 0, 'Multiplication Fails');
+        });
+
+        it('Multiply with 1', function() {
+            const result = calc.multiply(5, 1);
+            chai.assert.equal(result, 5, 'Multiplication Fails');
+        });
+
+        it('Multiply with number', function() {
+            const result = calc.multiply(5, 7);
+            chai.assert.equal(result, 35, 'Multiplication Fails');
+        });
+
+        it('Multiply with negative number', function() {
+            const result = calc.multiply(5, -5);
+            chai.assert.equal(result, -25, 'Multiplication Fails');
+        });
+
+        it('Multiply with both negatives', function() {
+            const result = calc.multiply(-5, -5);
+            chai.assert.equal(result, 25, 'Multiplication Fails');
+        });
+    });
+
+    describe('Division Calculation', function(){
+        it('Numerator larger than denominator', function() {
+            const result = calc.divide(9, 6);
+            chai.assert.equal(result, 1.5, 'Division Fails');
+        });
+
+        it('Numerator equal to denominator', function() {
+            const result = calc.divide(6, 6);
+            chai.assert.equal(result, 1, 'Division Fails');
+        });
+
+        it('Numerator smaller than denominator', function() {
+            const result = calc.divide(3, 6);
+            chai.assert.equal(result, 0.5, 'Division Fails');
+        });
+
+        it('Division by Zero', function() {
+            const result = calc.divide(3, 0);
+            chai.assert.equal(result, Infinity, 'Division Fails');
+        });
+    });
+});
